cea20d06c72d2521fdf208af59484085e1ec58b7
Alvin Ababil <neoryumasil@gmail.com>
2025-05-04 17:31:56 +0700

    Nambahin Collision Pada Sprite

--- schoolfellas/Script/playerMovement.gd
+++ schoolfellas/Script/playerMovement.gd
@@ -5,15 +5,39 @@ const SPEED = 300.0
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Variables
 	var direction := Input.get_axis("move_left", "move_right")
 	var updown := Input.get_axis("move_up", "move_down")
-	if direction:
+	var isMove = false
+	
+	# Buat Gerak Kiri/Kanan
+	if direction && !isMove:
 		velocity.x = direction * SPEED
+		isMove = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		isMove = false
 	
-	if updown:
+	# Buat Gerak Atas/Bawah
+	if updown && !isMove:
 		velocity.y = updown * SPEED
+		isMove = true
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		isMove = false
+	
+	# Animasi Gerak
+	if velocity.x != 0 && velocity.y == 0 :
+		if direction == 1:
+			$Animation.play("JalanKanan")
+		elif direction == -1:
+			$Animation.play("JalanKiri")
+	elif velocity.x == 0 && velocity.y != 0:
+		if updown == 1:
+			$Animation.play("JalanBawah")
+		elif updown == -1:
+			$Animation.play("JalanAtas")
+	else:
+		$Animation.pause()
+		
 	move_and_slide()
