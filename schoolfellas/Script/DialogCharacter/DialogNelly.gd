6b5aa4ee56d349b345dc1fa283ce0022d4a1774d
Alvin Ababil <neoryumasil@gmail.com>
2025-05-08 11:08:54 +0700

    update dialog Nelly

---
+++ schoolfellas/Script/DialogCharacter/DialogNelly.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# Day 1
+var dialogKelasD1 = [	"Hey Raka♡, Kamu bisa bantu aku gak membuang sampah dari piket pagi? Kebetulan kamu yang terakhir kali datang ke kelas?"]
+
+# Day 2
+var dialogKantinD2A = [	"RAKA!, kok lu bisa nemuin benda itu?",
+						"Eh... Aku sebenarnya aku ngga maksud membuangnya! Tapi aku terburu-buru karena aku ada urusan klub drama.",
+						"Oh..., Lu punya nyali ya nawarin pekerjaan?",
+						"Oke, bantu gua cari properti yang dibutuhin klub drama! Gua muak disuruh-suruh oleh ketua mata empat songong itu. Seluruh tugas yang dia kasih cuman untuk mengusirku dari ruangan klub. Dasar muka dua."]
+
+var dialogKlubD2A = [	"Apa sih tujuan lu sembunyiin TOPENG ku?",
+						"Jangan bohong! Aku tahu lu yang sembunyiin TOPENG ku?"]
+
+var dialogKlubD2B = [	"RAKA,..itu kan?",
+						"Tidak! Jangan percaya ke dia."]
+
+var dialogKantinD2B = [	"Aku minta maaf karena menjauh dari lu, WID",
+						"RAKA, aku juga berterimakasih kepadamu karena mencari KOSTUM drama milikku. Oh iya, nampaknya kamu WID WIDYA yang bakal jadi ketua Klub drama. Seluruh anggota terkesan dengan keberanian lu"]
+
+var dialogKlubD2C = [	"Maksud lu? Lu percaya cerita yang disebarkan RATNA",
+						"WIDYA! Tapi kan…",
+						"Aku juga akan keluar!"]
+
+var dialogLorongD2 = [	"Dulu, aku yang menyebabkan WIDYA menjadi seperti itu, awalnya kita hanya bercanda. Namun, WIDYA terdorong dan tertabrak oleh mobil. Karena hal itu, aku tidak berani dekat dengan dia.",
+						"Oh ya, terimakasih udah menemukan KOSTUM TOPENG milikku. Tapi, nampaknya tidak ada gunanya lagi",
+						"Hahaha, oh RAKA. garing amat sih"] 
