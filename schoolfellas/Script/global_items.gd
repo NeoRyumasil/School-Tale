f3e5e846644001c51f01d5fcc9eadc70e14647f7
Alvin Ababil <neoryumasil@gmail.com>
2025-05-09 22:35:41 +0700

    Buat Scene

--- schoolfellas/Script/global_items.gd
+++ schoolfellas/Script/global_items.gd
@@ -7,23 +7,9 @@ var isDay3 = false
 var isDay4 = false
 var isDay5 = false
 
-# Turns
-var isRakaTurns = false
-var isHeriTurns = false
-var isGunturTurns = false
-var isAndiTurns = false
-var isNellyTurns = false
-var isEdiTurns = false
-
 #Barang
 var haveTasHitam = false
-
-#isCheckpoint
-var isCheckpointParkiran = false
-var isCheckpointLorongEntrance = false
-var isCheckpointLorongR1 = false
-var isCheckpointLorongR2 = false
-var isCheckpointKelas = false
+var haveSampah = false
 
 #Checkpoint
 var checkpoint = Vector2.ZERO
