811ecfbed6339c2f91e65b81f6adb38d0141fb86
Alvin Ababil <neoryumasil@gmail.com>
2025-05-10 21:10:05 +0700

    Fix Bug Animasi

--- schoolfellas/Script/putra.gd
+++ schoolfellas/Script/putra.gd
@@ -7,6 +7,7 @@ var isChat = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Animation.play("Idle")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- schoolfellas/Script/ratna.gd
+++ schoolfellas/Script/ratna.gd
@@ -7,8 +7,9 @@ var isChat = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Animation.play("Idle")
 	pass
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Interact") && canInteract:
