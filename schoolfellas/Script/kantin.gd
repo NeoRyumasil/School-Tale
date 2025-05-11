aefc71b3698f0d83aec493265e3db25d7ca8cd06
Alvin Ababil <neoryumasil@gmail.com>
2025-05-11 17:18:21 +0700

    buat Kantini

---
+++ schoolfellas/Script/kantin.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var kios1
+var kios2
+var kios3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	kios1 = get_node("Object/Kios4/Sprite")
+	kios2 = get_node("Object/Kios5/Sprite")
+	kios3 = get_node("Object/Kios6/Sprite")
+	var objects = [kios1, kios2, kios3]
+	
+	for i in objects.size():
+		objects[i].flip_h = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
